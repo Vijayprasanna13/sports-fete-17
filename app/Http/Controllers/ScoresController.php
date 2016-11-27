e4d89c5c5a62fb4c98bdd4d8fac650c3744c66e4
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2016-11-27 14:59:11 +0530

    Add API method to ScoresController for GET: /api/log

--- app/Http/Controllers/ScoresController.php
+++ app/Http/Controllers/ScoresController.php
@@ -2,9 +2,27 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class ScoresController extends Controller{
+  use Validity;
   public function GetLog(Request $request){
-    $department_id = (app('db')->select('select id from departments where department_name = "'.(string)($request['department']).'"'))[0]->id;
-    $log = app('db')->select('select * from scores where department_id = '.$department_id.'');
-    return $log;
+    $data = [];
+    if(isset($request['department'])){
+      if($this->IsDepartmentValid($request['department'])){
+        $department_id = (app('db')->select('select id from departments where department_name = "'.(string)($request['department']).'"'))[0]->id;
+        $log = app('db')->select('select * from scores where department_id = '.$department_id.'');
+        $data['status'] = '200 OK';
+        $data['message'] = 'department scores logs found';
+        $data['data'] = $log;
+      }
+      else{
+        $data['status'] = '404 NOT FOUND';
+        $data['message'] = 'department not found';
+        $data['data'] = NULL;
+      }
+    }
+    else{
+      $data['status'] = '400 BAD REQUEST';
+      $data['message'] = 'missing params';
+    }
+    return json_encode($data);
   }
 }
