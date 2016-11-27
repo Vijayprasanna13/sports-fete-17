0965340bc81b6c38b90e10d871b6e8606fb79f98
rohithbalaji123 <rohithbalaji123@gmail.com>
2016-11-27 15:04:14 +0530

    Add PostPhotos method complete

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -6,29 +6,14 @@ use Illuminate\Http\Request;
 
 class PhotosController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    public function CheckId($id)
-    {
-        $found = app('db')->select('SELECT image_id FROM photos WHERE image_id = '.$id.'');
-        return $found;
-    }
+    use Validity;
 
     public function GetPhotos(Request $request, $id)
     {
         $data = [];
         $data['type'] = 'photos';
         $data['id'] = $id;
-        $data['count'] = $this->CheckId($id);
-        if($this->CheckId($id) !== []) {
+        if($this->IsImageIdValid($id)) {
             $data['status'] = '200 OK';
             $data['message'] = 'photo found';
             $image = app('db')->select('SELECT image_location FROM photos WHERE image_id = '.$id.'');
@@ -43,5 +28,31 @@ class PhotosController extends Controller
         return json_encode($data);
     }
 
-    //
+    public function PostPhotos(Request $request) {
+    	$data = [];
+    	if(isset($request['day']) && isset($request['image_location']) && IsDayValid($request['day'])) {
+    		$result = app('db')
+    		          ->insert('insert into
+    		                    photos (day, image_location, updated_at, created_at)
+    		                    values (
+    		                      "'.(string)$request['day'].'",
+    		                      "'.(string)$request['image_loaction'].'",
+    		                      "'.(string)date('Y-m-d H:i:s').'",
+    		                      "'.(string)date('Y-m-d H:i:s').'")
+    		                  ');
+    		if($result) {
+    			$data['status'] = '200 OK';
+    			$data['message'] = 'photo has been added';
+    		}
+    		else {
+    			$data['status'] = '500 ERROR';
+    			$data['message'] = 'internal error';
+    		}
+    	}
+    	else {
+    		$data['status'] = '400 BAD REQUEST';
+    		$data['message'] = 'missing params or invalid day';
+    	}
+    	return json_encode($data);
+    }
 }
