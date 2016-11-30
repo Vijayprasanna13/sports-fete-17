e4ccacf1f9828f5349baabf9d41feba52d5d07cf
rohithbalaji123 <rohithbalaji123@gmail.com>
2016-12-01 00:19:18 +0530

    Add loggedin.blade.php to add departments only(not final)

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,11 +1,10 @@
 <?php
 namespace App\Http\Controllers;
-session_start();
 class PagesController extends Controller{
   public function GetLoginView(){
     return view('auth.login');
   }
   public function GetAdminView(){
-    return $_SESSION['username'];
+    return view('auth.loggedin');
   }
 }
---
+++ public/js/loggedin.js
@@ -0,0 +1,32 @@
+var Login = React.createClass({
+  submit: function(){
+      $.ajax({
+        type: "POST",
+        url: '/api/scores',
+        data: {"department":$('#username').val(),"score":$('#password').val()},
+        success: function(data){
+          data = JSON.parse(data);
+          console.log(data['status']);
+          if(data['status'] == '200 Authorized')
+            window.location.href = '/auth/dashboard';
+        },
+        error: function(){console.log('error');}
+      });
+  },
+  render: function(){
+    return (
+      <div>
+        <div className="form-group">
+          <label htmlfor="username">Username:</label>
+          <input type="text" name="username" className="form-control" id="username" />
+        </div>
+        <div class="form-group">
+          <label htmlfor="password">Password:</label>
+          <input type="number" name="password" className="form-control" id="password" />
+        </div><br/>
+          <button onClick={this.submit} className="btn btn-success">Login</button>
+      </div>
+        );
+  }
+});
+  ReactDOM.render(<Login />,$('#login-form').get(0));
