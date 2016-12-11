b36a0484ddc195eb04df92d67bd4a75a2351de21
rohithbalaji123 <rohithbalaji123@gmail.com>
2016-12-11 12:56:22 +0530

    Change materialize to bootstrap

--- resources/views/index.php
+++ resources/views/index.php
@@ -3,174 +3,170 @@
     <head>
       <!--Import Google Icon Font-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+      <link type="text/css" rel="stylesheet" href="css/style.css">
+
+      <!--
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.6/handlebars.js"></script>
-
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      
+      -->
     </head>
 
     <body>
-      <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <nav class="navbar navbar-default" id="navbar">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavBar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand">Logo</a>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavBar">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Page1</a></li>
+              <li><a href="#">Page2</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#"><span class="glyphicon glyphicon-log-in"> Login</span></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-      <nav>
-    <div class="nav-wrapper red">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="">Sass</a></li>
-        <li><a href="">Components</a></li>
-        <li><a href="">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-3 table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th colspan="3" style="text-align: center; background-color: #DD4A3A">Leaderboard</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>#</td>
+                  <td>CSE</td>
+                  <td>00</td>
+                </tr>
+                <tr>
+                  <td>#</td>
+                  <td>CSE</td>
+                  <td>00</td>
+                </tr>
+                <tr>
+                  <td>#</td>
+                  <td>CSE</td>
+                  <td>00</td>
+                </tr>
+                <tr>
+                  <td>#</td>
+                  <td>CSE</td>
+                  <td>00</td>
+                </tr>
+                <tr>
+                  <td>#</td>
+                  <td>CSE</td>
+                  <td>00</td>
+                </tr>
+                <tr>
+                  <td>#</td>
+                  <td>CSE</td>
+                  <td>00</td>
+                </tr>
+                <tr>
+                  <td>#</td>
+                  <td>CSE</td>
+                  <td>00</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
-  <br>
-  <br>
-  <div class="container" >
-    <div class="row">
-  <div class="card col s3 l3" >
-    <div class="card red" id="leaderBoard" ><h5>Leader Board</h5></div>
-    <h5><div class="card red btn dept"><a href="#modal1">1</a></div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
-    <div class="divider"></div>
-    <h5><div class="card red btn dept" >1</div> CSE <a >00</a></h5>
+          <!-- Modal Structure -->
+          <div id="modal1" class="modal">
+            <div class="modal-content">
+              <h4>Modal Header</h4>
+              <p>A bunch of text</p>
+            </div>
+            <div class="modal-footer">
+              <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+            </div>
+          </div>
 
-  </div>
+          <div class="col-sm-8 col-lg-7 col-lg-offset-1 table-responsive">
+            <table class="table table-striped">
+              <thead style="background-color: #DD4A3A">
+                <tr>
+                  <th>Date</th>
+                  <th>Event</th>
+                  <th>Start</th>
+                  <th>End</th>
+                </tr>
+              </thead>
 
+              <tbody>
+                <tr>
+                  <td>1/1/2017</td>
+                  <td>Event Name</td>
+                  <td>00:00</td>
+                  <td>00:00</td>
+                </tr>
+                <tr>
+                  <td>1/1/2017</td>
+                  <td>Event Name</td>
+                  <td>00:00</td>
+                  <td>00:00</td>
+                </tr>
+                <tr>
+                  <td>1/1/2017</td>
+                  <td>Event Name</td>
+                  <td>00:00</td>
+                  <td>00:00</td>
+                </tr>
+                <tr>
+                  <td>1/1/2017</td>
+                  <td>Event Name</td>
+                  <td>00:00</td>
+                  <td>00:00</td>
+                </tr>
+                <tr>
+                  <td>1/1/2017</td>
+                  <td>Event Name</td>
+                  <td>00:00</td>
+                  <td>00:00</td>
+                </tr>
+                <tr>
+                  <td>1/1/2017</td>
+                  <td>Event Name</td>
+                  <td>00:00</td>
+                  <td>00:00</td>
+                </tr>
+                <tr>
+                  <td>1/1/2017</td>
+                  <td>Event Name</td>
+                  <td>00:00</td>
+                  <td>00:00</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
+        </div>
+      </div>
 
-  <!-- Modal Structure -->
-  <div id="modal1" class="modal">
-    <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>A bunch of text</p>
-    </div>
-    <div class="modal-footer">
-      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-    </div>
-  </div>
-
-
-  <div class="card col s8 l7 offset-l1 " >
-    <div class="card red eventHeader" >
-      <a class="date">Date</a>
-      <a class="eventName">Event</a>
-      <a class="startTime">Start</a>
-      <a class="endTime">End</a>
-
-
-  </div>
-  <div class="card grey lighten-1 events" >
-      <a class="date">1/1/2017</a>
-      <a class="eventName">Event Name</a>
-      <a class="startTime">00:00</a>
-      <a class="endTime">00:00</a>
-
-
-  </div>
-  <div class="card grey lighten-1 events" >
-      <a class="date">1/1/2017</a>
-      <a class="eventName">Event Name</a>
-      <a class="startTime">00:00</a>
-      <a class="endTime">00:00</a>
-
-
-  </div>
-  <div class="card green lighten-1 events" >
-      <a class="date">1/1/2017</a>
-      <a class="eventName">Event Name</a>
-      <a class="startTime">00:00</a>
-      <a class="endTime">00:00</a>
-
-
-  </div>
-  <div class="card green lighten-1 events" >
-      <a class="date">1/1/2017</a>
-      <a class="eventName">Event Name</a>
-      <a class="startTime">00:00</a>
-      <a class="endTime">00:00</a>
-
-
-  </div>
-  <div class="card green lighten-1 events" >
-      <a class="date">1/1/2017</a>
-      <a class="eventName">Event Name</a>
-      <a class="startTime">00:00</a>
-      <a class="endTime">00:00</a>
-
-
-  </div>
-  <div class="card red lighten-1 events" >
-      <a class="date">1/1/2017</a>
-      <a class="eventName">Event Name</a>
-      <a class="startTime">00:00</a>
-      <a class="endTime">00:00</a>
-
-
-  </div>
-  <div class="card red lighten-1 events" >
-      <a class="date">1/1/2017</a>
-      <a class="eventName">Event Name</a>
-      <a class="startTime">00:00</a>
-      <a class="endTime">00:00</a>
-
-
-  </div>
-  
-
-
-  </div>
-   
-</div>
-</div>
-
-
- <script>
-  
-
-   $(document).ready(function(){
-      $('.carousel').carousel();
-      //$('.modal').modal();
-$('#modal1').modal('open');
-    });
-
-  </script>
-
-  <script type="text/JavaScript">
-
-  </script>
-
-
-    </body>
-  </html>
-
-
-
-
-
-
-
-
+      <script>
+        $(document).ready(function(){
+          $('.carousel').carousel();
+          //$('.modal').modal();
+          $('#modal1').modal('open');
+        });
+      </script>
+  </body>
+</html>
