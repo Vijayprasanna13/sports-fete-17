de11ad86913efd4eebe5de0e7038677c2a1fd521
rohithbalaji123 <rohithbalaji123@gmail.com>
2017-01-12 17:10:44 +0530

    Modify ScoresController, remove nested if else statements

--- app/Http/Controllers/ScoresController.php
+++ app/Http/Controllers/ScoresController.php
@@ -10,23 +10,24 @@ class ScoresController extends Controller{
   //request params: department
   public function GetLog(Request $request){
     $data = [];
-    if(isset($request['department'])){
-      if($this->isDepartmentValid($request['department'])){
-        $department = $request['department'];
-        $data['status'] = '200 OK';
-        $data['message'] = 'department scores logs found';
-        $data['data'] = Score::getDepartmentScore($department);}
-      else{
-        $data['status'] = '404 NOT FOUND';
-        $data['message'] = 'department not found';
-        $data['data'] = NULL;
-      }
-    }
-    else{
+    if(!isset($request['department'])) {
       $data['status'] = '400 BAD REQUEST';
       $data['message'] = 'missing params';
-      $data['data'] = NULL;
+      return json_encode($data);
     }
+
+    $department = $request['department'];
+
+    if(!$this->isDepartmentValid($request['department_id'])) {
+      $data['status'] = '404 NOT FOUND';
+      $data['message'] = 'department not found';
+      $data['data'] = NULL;
+      return json_encode($data);
+    }
+
+    $data['status'] = '200 OK';
+    $data['message'] = 'department scores logs found';
+    $data['data'] = Score::getDepartmentScore($department);
     return json_encode($data);
   }
 
