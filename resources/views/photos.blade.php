aa9be41b3ad5720006d32bdd0b4b0946672c6f39
rohithbalaji123 <rohithbalaji123@gmail.com>
2017-01-14 23:53:03 +0530

    Add galereya plugin for photos page

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Photos')
 
 @section('body')
+<!--
   <style media="screen">
     .navbar-default {
       background-color: #7777ff;
@@ -29,12 +30,148 @@
         height: auto !important;
       }
     }
+  </style>-->
+  <style media="screen">
+    body {
+      background: #f6f6f6;
+      color: #212121;
+    }
+    #container {
+      width: 100%;
+      height: 100vh;
+    }
   </style>
+  <link rel="stylesheet" href="/css/jquery.galereya.css">
+  <!--[if lt IE 9]> <link rel="stylesheet" href="/css/jquery.galereya.ie.css"> <![endif]-->
   <body>
 @endsection
 
 @section('content')
-<div class="container" id="photos">
+<div id="container">
+<div id="gallery" style="padding-top: 15vh;">
+  <img src="images/image1.png"
+        alt="Image"
+        data-desc="Example"
+        data-fullsrc="images/image1.png"
+    />
+    <img src="images/image3.jpg"
+        alt="Image"
+        data-desc="Example"
+        data-fullsrc="images/image3.jpg"
+    />
+    <img src="images/image4.jpg"
+        alt="Image"
+        data-desc="Example"
+        data-fullsrc="images/image4.jpg"
+    />
+    <img src="images/image5.jpg"
+        alt="Image"
+        data-desc="Example"
+        data-fullsrc="images/image5.jpg"
+    />
+    <img src="images/image5.jpg"
+        alt="Image"
+        data-desc="Example"
+        data-fullsrc="images/image5.jpg"
+    />
+    <img src="images/image6.jpg"
+        alt="Image"
+        data-desc="Example"
+        data-fullsrc="images/image6.jpg"
+    />
+    <img src="images/image1.png"
+          alt="Image"
+          data-desc="Example"
+          data-fullsrc="images/image1.png"
+      />
+      <img src="images/image3.jpg"
+          alt="Image"
+          data-desc="Example"
+          data-fullsrc="images/image3.jpg"
+      />
+      <img src="images/image4.jpg"
+          alt="Image"
+          data-desc="Example"
+          data-fullsrc="images/image4.jpg"
+      />
+      <img src="images/image5.jpg"
+          alt="Image"
+          data-desc="Example"
+          data-fullsrc="images/image5.jpg"
+      />
+      <img src="images/image5.jpg"
+          alt="Image"
+          data-desc="Example"
+          data-fullsrc="images/image5.jpg"
+      />
+      <img src="images/image6.jpg"
+          alt="Image"
+          data-desc="Example"
+          data-fullsrc="images/image6.jpg"
+      />
+      <img src="images/image1.png"
+            alt="Image"
+            data-desc="Example"
+            data-fullsrc="images/image1.png"
+        />
+        <img src="images/image3.jpg"
+            alt="Image"
+            data-desc="Example"
+            data-fullsrc="images/image3.jpg"
+        />
+        <img src="images/image4.jpg"
+            alt="Image"
+            data-desc="Example"
+            data-fullsrc="images/image4.jpg"
+        />
+        <img src="images/image5.jpg"
+            alt="Image"
+            data-desc="Example"
+            data-fullsrc="images/image5.jpg"
+        />
+        <img src="images/image5.jpg"
+            alt="Image"
+            data-desc="Example"
+            data-fullsrc="images/image5.jpg"
+        />
+        <img src="images/image6.jpg"
+            alt="Image"
+            data-desc="Example"
+            data-fullsrc="images/image6.jpg"
+        />
+        <img src="images/image1.png"
+              alt="Image"
+              data-desc="Example"
+              data-fullsrc="images/image1.png"
+          />
+          <img src="images/image3.jpg"
+              alt="Image"
+              data-desc="Example"
+              data-fullsrc="images/image3.jpg"
+          />
+          <img src="images/image4.jpg"
+              alt="Image"
+              data-desc="Example"
+              data-fullsrc="images/image4.jpg"
+          />
+          <img src="images/image5.jpg"
+              alt="Image"
+              data-desc="Example"
+              data-fullsrc="images/image5.jpg"
+          />
+          <img src="images/image5.jpg"
+              alt="Image"
+              data-desc="Example"
+              data-fullsrc="images/image5.jpg"
+          />
+          <img src="images/image6.jpg"
+              alt="Image"
+              data-desc="Example"
+              data-fullsrc="images/image6.jpg"
+          />
+</div>
+</div>
+<!--<div class="container" id="photos">
   <div class="row">
     <a href="images/image1.png" target="_blank">
       <div class="col-sm-3">
@@ -69,5 +206,13 @@
       </div>
     </a>
   </div>
-</div>
+</div>-->
+<script type="text/javascript" src="/js/jquery.galereya.min.js"></script>
+<script type="text/javascript">
+  $(function() {
+    $('#gallery').galereya({
+      slideShowSpeed: 10000
+    });
+  });
+</script>
 @endsection
