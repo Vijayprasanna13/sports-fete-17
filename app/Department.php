9673eeeced8b77053eded67f150e75a3d7e34e76
rohithbalaji123 <rohithbalaji123@gmail.com>
2017-01-15 15:05:49 +0530

    Add query to calculate scores of department from scores table

--- app/Department.php
+++ app/Department.php
@@ -3,10 +3,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Department extends Model{
   protected $table = "departments";
   public static function scores(){
+    return Department::join('scores', 'scores.department_id', '=', 'departments.id')
+                        ->select(DB::raw('departments.id, departments.department_name, SUM(scores.score) as score'))
+                        ->orderBy('score', 'desc')
+                        ->get();
     return Department::select('id','department_name','score')->orderByRaw('CAST(score AS DECIMAL(5,2)) DESC')->get();
   }
   public static function updateScore(Request $request){
