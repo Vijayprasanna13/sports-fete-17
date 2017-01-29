7973cbf4750af2bcd39fffdccd1bf75d08fb5e18
Priyanka Pal <priyankapal330@gmail.com>
2017-01-29 22:40:07 +0530

    add new carousel code

--- public/js/jquery.galereya.js
+++ public/js/jquery.galereya.js
@@ -1,5 +1,5 @@
-/**
- * Galereya v0.9.93
+/*!
+ * Galereya v0.9.94
  * http://vodkabears.github.com/galereya
  *
  * Licensed under the MIT license
@@ -32,8 +32,10 @@
             slides = [],
             htmlOverflow, bodyOverflow,
             gridW,
-            rowCount = 0,
+            gridH,
+            colCount = 0,
             cellW = 300,
+            cellH = 300,
             scrollTop,
             loadTimeout,
             currentSlideIndex,
@@ -187,13 +189,15 @@
          */
         var calcParams = function () {
             cellW = $cells.width();
+            cellH = $cells.height();
 
-            rowCount = Math.floor(self.width() / (cellW + self.options.spacing));
-            if (rowCount < 1) {
-                rowCount = 1;
+            colCount = Math.floor((self.width() - 10) / (cellW + self.options.spacing));
+            if (colCount < 1) {
+                colCount = 1;
             }
 
-            gridW = rowCount * cellW + (rowCount - 1) * self.options.spacing;
+            gridW = colCount * cellW + (colCount - 1) * self.options.spacing;
+            gridH = Math.ceil($imgs.length / colCount) * cellH;
         };
 
         /**
@@ -399,15 +403,22 @@
          * @param number
          */
         var placeCell = function (cell, number) {
-            var left, top, topCell, row;
+            var left, top, topCell, col;
 
-            row = number % rowCount;
-            left = row * cellW + self.options.spacing * row;
-            if (number >= rowCount) {
-                topCell = visibleCells[number - rowCount];
+            col = number % colCount;
+            left = col * cellW + self.options.spacing * col;
+            if (number >= colCount) {
+                topCell = visibleCells[number - colCount];
                 top = topCell.offsetTop + topCell.offsetHeight + self.options.spacing;
             } else {
                 top = 0;
+            }
+
+            // Change height cell by cell
+            var height = top + cell.offsetHeight;
+            if (height > gridH) {
+                gridH = height;
+                $grid.height(gridH + $grid[0].offsetTop);
             }
 
             cell.style.top = top + 'px';
@@ -419,6 +430,7 @@
          */
         var hideCells = function () {
             visibleCells = [];
+            gridH = 0;
             $cells.stop(true, true).fadeOut(200).removeClass('visible');
         };
 
@@ -476,7 +488,7 @@
             }
             $currentImg.css('margin-top', ($(window).height() - $currentImg.height()) / 2);
             $grid.width(gridW);
-
+            gridH = 0;
             for (var i = 0, len = visibleCells.length; i < len; i++) {
                 placeCell(visibleCells[i], i);
             }
