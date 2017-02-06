24a7b99d70753b020e01b956af86f8605a2902d5
rohithbalaji123 <rohithbalaji123@gmail.com>
2017-02-06 11:37:44 +0530

    Fix allignment of dept wise events table

--- resources/views/deptscore.blade.php
+++ resources/views/deptscore.blade.php
@@ -67,9 +67,9 @@
         <!-- /.container -->
     </nav>
 
-  <div class="container-fluid" id="departmentScoreboard" style="padding-top: 5vh;">
+  <div class="container" id="departmentScoreboard" style="padding-top: 5vh;">
     <div class="row">
-      <div class="col-sm-10 col-sm-offset-2 table-responsive">
+      <div class="col-sm-12 table-responsive">
         <table class="table table-striped text-center">
           <thead>
             <tr>
