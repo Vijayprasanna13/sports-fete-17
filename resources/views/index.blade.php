ae3325561839c8ff4fae1e8c7b56f0ad16dd8345
rohithbalaji123 <rohithbalaji123@gmail.com>
2017-02-19 11:11:13 +0530

    Change time of marathon from 5am to 5pm

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -338,8 +338,8 @@
                     </div>
                     <div class="row table-responsive">
 
-                    
-                        
+
+
 
                         <div class="col-md-9" style="margin-top:40px;">
 
@@ -573,7 +573,7 @@
 
     //var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
     var deadline = "Tue Feb 23 2017 06:00:00 GMT+0530 (IST)";
-    var deadline2= "Sat Feb 21 2017 05:00:00 GMT+0530 (IST)";
+    var deadline2= "Sat Feb 21 2017 17:00:00 GMT+0530 (IST)";
 
     initializeClock('clockdiv', deadline);
     initializeClock('clockdiv2', deadline2);
